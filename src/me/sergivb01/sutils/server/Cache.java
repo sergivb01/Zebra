ceb6afa5e4831f744bec67cf39a67cb717515183
sergivb01 <sergivb01@gmail.com>
2018-02-08 22:43:59 +0100

    Started server system cache

---
+++ src/me/sergivb01/sutils/server/Cache.java
@@ -0,0 +1,16 @@
+package me.sergivb01.sutils.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cache {
+	public static List<Server> serverList = new ArrayList<>();
+
+	/*
+	 * TODO:
+	 * Every single fucking shit
+	 */
+
+
+
+}
