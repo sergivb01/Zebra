cb43a55bd32cb6925cd0fa6f2846792801c3c100
sergivb01 <sergivb01@gmail.com>
2018-02-10 22:58:15 +0100

    test cmd

--- src/me/sergivb01/sutils/commands/TestCommand.java
+++ src/me/sergivb01/sutils/commands/TestCommand.java
@@ -1,5 +1,6 @@
 package me.sergivb01.sutils.commands;
 
+import me.sergivb01.sutils.queue.QueueAPI;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,7 +8,17 @@ import org.bukkit.command.CommandSender;
 public class TestCommand implements CommandExecutor{
 
 	public boolean onCommand (CommandSender sender, Command command, String s, String[] args) {
-		sender.sendMessage("None");
+
+		String thing = args[0];
+		String player = args[1];
+		if(thing.equalsIgnoreCase("add")){
+			QueueAPI.addPlayer(player, args[2], Integer.parseInt(args[3]));
+		}else if(thing.equalsIgnoreCase("remove")){
+			QueueAPI.removePlayer(player);
+		}else{
+			QueueAPI.statusOf(player);
+		}
+
 		return true;
 	}
 
